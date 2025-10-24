a5c3b2947aa4a19a415be2ac2ff34a2f7eb97022
Yasser MESTAOUI <128167310+yas0se@users.noreply.github.com>
2025-10-24 15:58:42 +0100

    config client not fixef

---
+++ gestion-doc-config-repo/src/main/java/org/example/gestiondocconfigrepo/GestionDocConfigRepoApplication.java
@@ -0,0 +1,13 @@
+package org.example.gestiondocconfigrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDocConfigRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDocConfigRepoApplication.class, args);
+    }
+
+}
